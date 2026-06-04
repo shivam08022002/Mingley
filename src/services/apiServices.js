120935dcd7b699d840eb93b1753650b489f19d04
Shivam Yadav <shivam@Shivams-MacBook-Air.local>
2026-06-04 18:57:18 +0530

    stage changes

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -101,24 +101,6 @@ export const walletService = {
       throw error.response?.data || error.message;
     }
   },
-
-  createRazorpayOrder: async (packageId) => {
-    try {
-      const response = await api.post('/v1/wallet/razorpay/order', { packageId });
-      return response.data;
-    } catch (error) {
-      throw error.response?.data || error.message;
-    }
-  },
-
-  verifyRazorpayPayment: async (verifyData) => {
-    try {
-      const response = await api.post('/v1/wallet/razorpay/verify', verifyData);
-      return response.data;
-    } catch (error) {
-      throw error.response?.data || error.message;
-    }
-  },
 };
 
 export const discoverService = {
